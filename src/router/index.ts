2965cc917c705c7d1100208c2f035513b5ff4160
Dudarik <Dudarik@yandex.ru>
2024-04-10 19:47:30 +0500

    feat: base settings for prettier and base app structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "auth",
+    component: () =>
+      import(/* webpackChunkName: "auth" */ "../views/AuthVue.vue"),
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/analytic",
+    name: "analytic",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "analytic" */ "../views/AnalyticView.vue"),
   },
 ];
 
