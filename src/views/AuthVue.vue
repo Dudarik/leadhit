01c8366e751000e72fd5ee42abc8759c5b48ad0d
Dudarik <Dudarik@yandex.ru>
2024-04-10 20:02:52 +0500

    feat: add primevue config

--- src/views/AuthVue.vue
+++ src/views/AuthVue.vue
@@ -1,12 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <InputText />
+    <Button label="submit" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+
 export default defineComponent({
   name: "AuthView",
-  components: {},
+  components: { InputText, Button },
 });
 </script>
